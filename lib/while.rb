7c14390f77e2126fc338c7c9b510456340197271
Bruce Ruffin <bruffin1221@gmail.com>
2019-04-09 18:55:18 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,6 +2,6 @@ def using_while
   levitation_force = 6
   while levitation_force<10
     levitation_force +=1
-  puts "Wingardium Leviosa #{leviathon force}"
+  puts "Wingardium Leviosa #{leviathon_force}"
 end
 end
